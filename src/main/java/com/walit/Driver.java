56dfa80c41d446dabc5efb623a4597de6454babd
Jack Swindell <jackson1swindell@gmail.com>
2024-04-07 01:42:56 -0500

    init project

---
+++ src/main/java/com/walit/Driver.java
@@ -0,0 +1,9 @@
+package com.walit;
+
+public class Driver {
+
+    public static void main(String[] args) {
+        System.out.println("Working");
+        System.out.println("Also working");
+    }
+}
